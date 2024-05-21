a67e2bdd5fbe84a8d6027dbd2fd0b73749ffa3fa
Bruno Fabre <bruno.hfabre@gmail.com>
2024-05-21 15:32:24 -0300

    more fix

--- android/src/main/java/expo/modules/toopinfo/ExpoToopInfoModule.kt
+++ android/src/main/java/expo/modules/toopinfo/ExpoToopInfoModule.kt
@@ -10,6 +10,15 @@ import java.io.ByteArrayOutputStream
 import android.content.Context
 import android.telephony.TelephonyManager
 import android.content.pm.PackageManager
+import android.app.admin.DevicePolicyManager
+import android.net.Uri
+import android.graphics.drawable.Drawable
+import org.json.JSONObject
+import android.graphics.drawable.AdaptiveIconDrawable
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.LayerDrawable
+import android.app.WallpaperManager
+import android.graphics.ImageDecoder
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
@@ -50,42 +59,59 @@ class ExpoToopInfoModule : Module() {
 
     Function("getInstalledPackages") {
       try {
+        val packageManager = getPackageManager()
+
         val intent = Intent(Intent.ACTION_MAIN, null)
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
 
-        val apps = getPackageManager().queryIntentActivities(intent, 0);
+        val items = packageManager.queryIntentActivities(intent, 0);
 
-        var allApps = emptyArray<String>()
+        var applications = emptyArray<String>()
 
-        for (item in apps) {
-          val appName = item.loadLabel(getPackageManager()).toString()
-          val packageName = item.activityInfo.packageName
-          val versionCode = getPackageManager().getPackageInfo(packageName, 0).getLongVersionCode()
+        for(item in items) {
+          val applicationInfo = packageManager.getPackageInfo(item.activityInfo.packageName, 0)
 
-          var iconDrawable = item.activityInfo.loadIcon(getPackageManager())
-          var icon: Bitmap
+          var icon = ""
 
-          if(iconDrawable.getIntrinsicWidth() <= 0 || iconDrawable.getIntrinsicHeight() <= 0) {
-            icon = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
-          } else {
-            icon = Bitmap.createBitmap(iconDrawable.getIntrinsicWidth(), iconDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888)
+          val drawable: Drawable = packageManager.getApplicationIcon(item.activityInfo.packageName)
+
+          if(drawable is BitmapDrawable) {
+            val outputStream = ByteArrayOutputStream()
+            drawable.bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+
+            icon = Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT)
+          } else if (drawable is AdaptiveIconDrawable) {
+            val backgroundDr = drawable.background
+            val foregroundDr = drawable.foreground
+
+            val drr = arrayOfNulls<Drawable>(2)
+            drr[0] = backgroundDr
+            drr[1] = foregroundDr
+
+            val layerDrawable = LayerDrawable(drr)
+
+            val width = layerDrawable.intrinsicWidth
+            val height = layerDrawable.intrinsicHeight
+
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+            val canvas = Canvas(bitmap)
+
+            layerDrawable.setBounds(0, 0, canvas.width, canvas.height)
+            layerDrawable.draw(canvas)
+
+            val outputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+
+            icon = Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT)
           }
-          
-          val canvas = Canvas(icon)
-          iconDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
-          iconDrawable.draw(canvas)
 
-          val byteArrayOutputStream = ByteArrayOutputStream()
-          icon.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
-          val byteArray = byteArrayOutputStream.toByteArray()
-          val encoded = Base64.encodeToString(byteArray, Base64.NO_WRAP)
-          
-          val appInfo = appName + ";" + packageName + ";" + versionCode + ";" + encoded
+          val application = applicationInfo.applicationInfo.loadLabel(packageManager).toString() + ";" + applicationInfo.packageName + ";" + applicationInfo.versionName + ";" + applicationInfo.longVersionCode.toString() + ";" + applicationInfo.firstInstallTime + ";" + applicationInfo.lastUpdateTime + ";" + icon
 
-          allApps = allApps + appInfo
+          applications = applications + application
         }
 
-        allApps
+        applications
       } catch (e: Exception) {
         "not-permitted"
       }
@@ -120,6 +146,48 @@ class ExpoToopInfoModule : Module() {
         "not-permitted"
       }
     }
+
+    Function("getEnrollmentSpecificId") {
+      try {
+        getDevicePolicyManager().getEnrollmentSpecificId()
+      } catch (e: Exception) {
+        "not-permitted"
+      }
+    }
+
+    Function("getDeviceId") {
+      try {
+        val uri = Uri.parse("content://com.google.android.gsf.gservices")
+
+        val query = context.contentResolver.query(uri, null, null, arrayOf<String>("android_id"), null)
+
+        if (query == null) {
+          return@Function "not-found"
+        }
+
+        if (!query.moveToFirst() || query.columnCount < 2) {
+          return@Function "not-found"
+        }
+
+        val toHexString = java.lang.Long.toHexString(query.getString(1).toLong())
+
+        query.close()
+
+        return@Function toHexString
+      } catch (e: Exception) {
+        "not-permitted"
+      }
+    }
+
+    // Function("setWallpaper") { uri: String ->
+    //   try {
+    //     val bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, Uri.parse(uri)))
+
+    //     WallpaperManager.getInstance(context).setBitmap(bitmap)
+    //   } catch (e: Exception) {
+    //     "not-permitted"
+    //   }
+    // }
   }
 
   private val context
@@ -131,5 +199,9 @@ class ExpoToopInfoModule : Module() {
 
   private fun getPackageManager(): PackageManager {
     return context.packageManager
+  }
+
+  private fun getDevicePolicyManager(): DevicePolicyManager {
+    return context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import ExpoToopInfoModule from "./ExpoToopInfoModule";
 
+export type Application = {
+  name: string;
+  packageName: string;
+  versionName: string;
+  versionCode: number;
+  firstInstallTime: string;
+  lastUpdateTime: string;
+  icon: string;
+}
+
 function returnResponse(result: any): any {
   if (result === 'not-permitted') {
     throw new Error('toop-info-not-permitted-error')
@@ -20,8 +30,32 @@ export function getSerialNumber(): string {
   return returnResponse(ExpoToopInfoModule.getSerialNumber());
 }
 
-export function getInstalledPackages(): string[] {
-  return returnResponse(ExpoToopInfoModule.getInstalledPackages());
+export function getInstalledPackages(supressPackages: string[]): Application[] {
+  const items = returnResponse(ExpoToopInfoModule.getInstalledPackages());
+
+  const applications = items.map((item: string) => {
+    const [
+      name,
+      packageName,
+      versionName,
+      versionCode,
+      firstInstallTime,
+      lastUpdateTime,
+      icon,
+    ] = item.split(';')
+
+    return {
+      name,
+      packageName,
+      versionCode: Number(versionCode),
+      versionName,
+      firstInstallTime,
+      lastUpdateTime,
+      icon: `data:image/png;base64,${icon}`
+    }
+  }).filter((application: Application) => !supressPackages.some(packageName => packageName === application.packageName))
+
+  return applications
 }
 
 export function sendActivityResultOk(): string {
@@ -31,3 +65,15 @@ export function sendActivityResultOk(): string {
 export function launchApplication(packageName: string): string {
   return returnResponse(ExpoToopInfoModule.launchApplication(packageName));
 }
+
+export function getEnrollmentSpecificId(): string {
+  return returnResponse(ExpoToopInfoModule.getEnrollmentSpecificId());
+}
+
+export function getDeviceId(): string {
+  return returnResponse(ExpoToopInfoModule.getDeviceId());
+}
+
+// export function setWallpaper(uri: string): string {
+//   return returnResponse(ExpoToopInfoModule.setWallpaper(uri));
+// }
